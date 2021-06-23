fd635e3358e1194f34826bd227cd50b546b72d4c
liuduan <liuduan@fenbi.com>
2021-06-23 10:52:25 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 // 新建 reactMiniBook.js, 运行 node reactMiniBook.js 生成pdf
 const puppeteer = require('puppeteer');
 
+const timeout = (s) => new Promise((resolve) => {
+    setTimeout(() => {
+        resolve();
+    }, s || 0);
+});
+
 (async () => {
     // 启动浏览器
     const browser = await puppeteer.launch({
@@ -12,30 +18,41 @@ const puppeteer = require('puppeteer');
     // 打开一个标签页
     const page = await browser.newPage();
 
-    const args = 1;
-    let wh = await page.evaluate((args) => {
-        // args 可以这样传递给这个函数。
-        // 类似于 setTimeout(() => {console.log(args);}, 3000, args);
-        console.log('args', args); // 1
-        // 这里可以运行 dom操作等js
-        // 返回通过dom操作等获取到的数据
-        return {
-            width: 1920,
-            height: document.body.clientHeight,
-        };
-    }, args);
-    // 设置视图大小
-    // await page.setViewport(wh);
-    // 等待2s
-    // await page.waitFor(2000);
-
-
-    await page.goto(
-        'https://mp.weixin.qq.com/s/xKppz1O-L4rYiNpOaUb0SQ',
-        { waitUntil: 'networkidle2' }
-    );
     // 以iPhone 11执行
     await page.emulate(puppeteer.devices['iPhone 11']);
+
+    await page.goto(
+        'http://es6.ruanyifeng.com/#README',
+        { waitUntil: 'networkidle2' }
+    );
+
+    await timeout(2000);
+
+    // page.evaluate 回调在可执行浏览器环境中代码逻辑，如 dom 操作等
+    let aTags = await page.evaluate(() => {
+        // 这里的 console 打印看不到，因为回调是在浏览器环境中打印的，非 node 环境
+        let as = [...document.querySelectorAll('ol li a')];
+        return as.map((a) => {
+            return {
+                href: a.href.trim(),
+                name: a.text
+            }
+        });
+    });
+
+    // let wh = await page.evaluate((args) => {
+    //     // args 可以这样传递给这个函数。
+    //     // 类似于 setTimeout(() => {console.log(args);}, 3000, args);
+    //     console.log('args=====', args, document); // 1
+    //     // 这里可以运行 dom操作等js
+    //     // 返回通过dom操作等获取到的数据
+    //     return {
+    //         width: 1920,
+    //         height: document.body.clientHeight,
+    //     };
+    // });
+    // // 设置视图大小
+    // await page.setViewport(wh);
 
     // path 路径， format 生成pdf页面格式
     await page.pdf({ path: 'rxjs.pdf', format: 'A4' });
